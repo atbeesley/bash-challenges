aa9bd99c484fdab17149a5ab9891b01592c68234
atbeesley <alexander.beesley@gmail.com>
2020-09-28 22:36:07 +0100

    adds more on conditionals

---
+++ more_on_conditionals.sh
@@ -0,0 +1,22 @@
+# Given three integers (x, y, and z) representing the three sides of a triangle, 
+#
+# identify whether the triangle is scalene, isosceles, or equilateral.
+#
+# If all three sides are equal, output EQUILATERAL.
+# Otherwise, if any two sides are equal, output ISOSCELES.
+# Otherwise, output SCALENE.
+
+
+read x;
+read y;
+read z;
+
+if (($x == $y)) && (($y == $z)) && (($x == $z));
+then
+echo "EQUILATERAL";
+elif (($x == $y || $y == $z || $x == $z ));
+then
+echo "ISOSCELES";
+else
+echo "SCALENE";
+fi
