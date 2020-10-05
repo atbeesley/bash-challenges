d8df76e6a1827b4f73e4d4895203fcd82accd9b5
atbeesley <alexander.beesley@gmail.com>
2020-10-05 15:50:19 +0100

    adds head_text_file_2

---
+++ head_of_a_text_file_2.sh
@@ -0,0 +1,11 @@
+# In this challenge, 
+#
+# we practice using the head command
+#
+# to display the first N characters of a text file.
+#
+#
+# Display the first 20 characters of an input file.
+
+
+head -c20
