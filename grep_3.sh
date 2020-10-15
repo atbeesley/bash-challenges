c1755460a73ada0e1f909197f3acdbe017755e9f
atbeesley <alexander.beesley@gmail.com>
2020-10-15 16:11:52 +0100

    adds grep_3

---
+++ grep_3.sh
@@ -0,0 +1,7 @@
+# You are given a text file that will be piped into your command through STDIN. 
+#
+# Use grep to remove all those lines that contain the word 'that'.
+#
+# The search should NOT be sensitive to case.
+
+grep -vi 'that'
