5e090c19d41c20d4771a8b9bbbf32b68ce23d0e3
atbeesley <alexander.beesley@gmail.com>
2020-10-21 13:38:44 +0200

    adds sed_3

---
+++ sed_3.sh
@@ -0,0 +1,9 @@
+# Given an input file, 
+#
+# in each line, highlight all the occurrences of 'thy'
+#
+# by wrapping them up in brace brackets.
+#
+# The search should be case-insensitive.
+
+sed -e 's/thy/{&}/gi'
